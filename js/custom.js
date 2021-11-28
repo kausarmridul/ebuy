37e3fd156427f141ee7b1be4d391a07543efe5d7
MLMridul <mdmridul344@gmail.com>
2021-11-28 19:19:14 +0600

    Code Add

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,20 @@ $(document).ready(function(){
     'use strict'
     $('.service-body').slick({
         slidesToShow: 3,
-        // slidesToScroll: 1,
-        arrows:false,
+        arrows:false,  
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            }]
     });
+    $('.banner-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll:1,
+      autoplay:true,
+      arrows:false,  })
 })
