f329d6f371b9299f1e4df37f05d1be4cf4c1a7d7
MLMridul <mdmridul344@gmail.com>
2021-11-29 02:15:57 +0600

    Code Add

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,29 @@
-$(document).ready(function(){
-    'use strict'
-    $('.service-body').slick({
-        slidesToShow: 3,
-        arrows:false,  
-        responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-              }
-            }]
-    });
-    $('.banner-slider').slick({
-      slidesToShow: 1,
-      slidesToScroll:1,
-      autoplay:true,
-      arrows:false,  })
+$(document).ready(function () {
+  'use strict'
+  $('.service-body').slick({
+    slidesToShow: 3,
+    arrows: false,
+    responsive: [{
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    }]
+  });
+  $('.banner-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+  });
+  $('#countdown').countdown({
+    year: 2022, // YYYY Format
+    month: 1, // 1-12
+    day: 1, // 1-31
+    hour: 0, // 24 hour format 0-23
+    minute: 0, // 0-59
+    second: 0, // 0-59
+  });
 })
