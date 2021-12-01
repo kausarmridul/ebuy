dd1fecc43cc508aa84f5fe5aac859515441c91d4
MLMridul <mdmridul344@gmail.com>
2021-12-02 02:35:30 +0600

    Code Add

--- js/aboutpage.js
+++ js/aboutpage.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     'use strict'
+
+    // Designer Slider
+
+    $('.designer-slider').slick({
+        slidesToShow: 4,
+        arrows: false,
+
+    });
+
+    // Counter Up
     $('.counter').counterUp({
         delay: 10,
         time: 1000
